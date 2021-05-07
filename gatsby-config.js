4845678ed00e7ae71899b98a5cf891795d33c946
Luca Bruzzone <spam@unsigned.it>
2021-05-07 10:41:02 +0200

    more bumps

--- gatsby-config.js
+++ gatsby-config.js
@@ -130,7 +130,6 @@ module.exports = {
     },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
-    'gatsby-plugin-netlify',
     {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
